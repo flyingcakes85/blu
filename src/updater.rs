592917821a08deda6b7dad65f81308d8193b03d3
Snehit Sah <snehitsah@protonmail.com>
2022-01-26 19:00:58 +0530

    Add Hash update skel
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- src/updater.rs
+++ src/updater.rs
@@ -1,6 +1,19 @@
+use serde_json::Value as JsonValue;
 use std::error::Error;
-use toml::Value;
+use std::fs;
+use toml::Value as TomlValue;
 
-pub fn process_sources(_source_opts: &Value) -> Result<(), Box<dyn Error>> {
+pub fn process_sources(source_opts: &TomlValue) -> Result<(), Box<dyn Error>> {
+    let updates = fs::read_to_string(source_opts["updates"].as_str().unwrap())
+        .expect("Updates file does not exist. Check config file.")
+        .parse::<JsonValue>()
+        .expect("Could not parse updates file.");
+    let updates = updates["updates"].as_array().unwrap();
+
+    for update in updates {
+        // update empty hashes
+        println!("{:#?}", update);
+    }
+
     Ok(())
 }
